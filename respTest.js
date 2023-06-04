a408a5aa17848ae4f4d26454a956320cb3c0e1c9
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-06-04 21:07:58 +0530

    remove function arguments

--- respTest.js
+++ respTest.js
@@ -2,19 +2,19 @@ const test = require("node:test");
 const assert = require("assert");
 const resp = require('./resp');
 
-test("Decode Simple String", (t) => {
+test("Decode Simple String", () => {
     respDecoder = new resp.RESP();
     buffer = Buffer.from("+OK\r\n");
     assert.strictEqual(respDecoder.decode(buffer), "OK");
 });
 
-test("Decode Error", (t) => {
+test("Decode Error", () => {
     respDecoder = new resp.RESP();
     buffer = Buffer.from("-Error message\r\n");
     assert.strictEqual(respDecoder.decode(buffer), "Error message");
 });
 
-test("Decode Integer", (t) => {
+test("Decode Integer", () => {
     respDecoder = new resp.RESP();
     buffer = Buffer.from(":1000\r\n");
     assert.strictEqual(respDecoder.decode(buffer), 1000);
